72aee17ba92ac2ffbb4b660adf6f74cff24d01b2
rtilwalia <tilwalia.ritika01@gmail.com>
2021-08-25 19:36:08 +0530

    final project

--- Activity/commands/tree.js
+++ Activity/commands/tree.js
@@ -1,10 +1,23 @@
 //folderpath-> traverse the file/folder then print them
 
-treeFunction(path){
+let fs = require('fs');
+let path = require('path');
 
+function treeFn(srcpath){
+    let baseName = path.basename(srcpath)
+    console.log(baseName);
+    console.log("├──")
+    let content = fs.readFileSync(srcpath);
+    console.log(content);
+
+    let allEntities = "";
+    for(let i = 0; i < content.length; i++){
+        allEntities += '\n\t' + "├──" + content[i];
+        console.log(content[i]);
+    }
 }
 
 
 module.exports = {
-    treeFn : treeFunction
-};
+    treeFn : treeFn
+}
